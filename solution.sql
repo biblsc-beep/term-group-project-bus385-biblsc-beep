7b798d013d90b6bd9251728706400aa9310e86c4
biblsc-beep <biblsc@farmingdale.edu>
2026-04-26 20:57:51 -0400

    Update solution.sql
    Signed-off-by: biblsc-beep <biblsc@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,1 +1,8 @@
--- Write your SQL query below:
+SELECT Artists.ArtistName,
+       COUNT(Streams.StreamID) AS TotalStreams
+FROM Artists
+JOIN Albums ON Artists.ArtistID = Albums.ArtistID
+JOIN Songs ON Albums.AlbumID = Songs.AlbumID
+JOIN Streams ON Songs.SongID = Streams.SongID
+GROUP BY Artists.ArtistName
+ORDER BY TotalStreams DESC;
