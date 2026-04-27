555f957d6c9aa78d1d6db9bc387f1a6e6e9f8b7e
biblsc-beep <biblsc@farmingdale.edu>
2026-04-26 20:46:23 -0400

    Update solution.sql
    Signed-off-by: biblsc-beep <biblsc@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,1 +1,9 @@
 -- Write your SQL query below:
+SELECT a.ArtistName,
+COUNT(s.StreamID) AS TotalStreams,
+SUM(r.RatePerStream * (r.PercentageRate / 100.0)) AS TotalRoyalty
+FROM Artists a
+JOIN RoyaltyRates r ON a.ArtistID = r.ArtistID
+JOIN Streams s ON r.SongID = s.SongID
+GROUP BY a.ArtistName
+ORDER BY TotalRoyalty DESC;
